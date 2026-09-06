2dd16c28a3dac49e85dfad55c6a9e4bd7a6b8a04
Meet Miglani <meetmiglani1@gmail.com>
2026-09-07 01:06:52 +0530

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,5 +1,10 @@
-from sqlmodel import create_engine
+from sqlmodel import SQLModel, create_engine
 
 from app.config import settings
 
 engine = create_engine(settings.database_url, echo=settings.debug)
+
+
+def init_db():
+    from app.models import db_models  # noqa: F401
+    SQLModel.metadata.create_all(engine)
